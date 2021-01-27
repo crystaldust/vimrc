4132fe84445a38d9848400a33cf3b3b0c62f67a3
crystaldust <juzhenatpku@gmail.com>
2021-01-27 18:16:26 +0800

    Make up the setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git submodule update --init --recursive
+cp vimrc $HOME/.vimrc
+cp -r molokai/* $HOME/.vim/
+cp -r solarized/* $HOME/.vim/
